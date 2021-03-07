ff44fb0e42dc2ac97b6a8c7802052083d345ddd7
Aidan Lineberry <aidan.lineberry@gmail.com>
2021-03-07 03:25:06 -0600

    Updates

---
+++ precache-manifest.2f59ec8b53342a3d1835a00695e95272.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4a2ef56be3b48df2c816339ed727242",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3425b95c76c07cc3ccad",
+    "url": "/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "ca2505c0ada59326819c",
+    "url": "/static/css/main.a1b7b015.chunk.css"
+  },
+  {
+    "revision": "3425b95c76c07cc3ccad",
+    "url": "/static/js/2.c459719d.chunk.js"
+  },
+  {
+    "revision": "ca2505c0ada59326819c",
+    "url": "/static/js/main.0b194353.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "be6cd5f38d11fc6ddfe926a50369d3d4",
+    "url": "/static/media/ecofriendly.be6cd5f3.webp"
+  },
+  {
+    "revision": "fc2b438cd3069012405ee0522189e013",
+    "url": "/static/media/instagram.fc2b438c.webp"
+  },
+  {
+    "revision": "95bf7307929c3e4487c340522abfb3ce",
+    "url": "/static/media/opensource.95bf7307.webp"
+  }
+]);
